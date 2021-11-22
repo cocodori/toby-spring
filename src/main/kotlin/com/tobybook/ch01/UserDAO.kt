4cda45fca4e50f19f98b143f934c264bab364133
hoon <moradulee@gmail.com>
2021-11-23 00:27:16 +0900

    jdbcTemplate으로 변경 + getAll() 메소드 추가

--- src/main/kotlin/com/tobybook/ch01/UserDAO.kt
+++ src/main/kotlin/com/tobybook/ch01/UserDAO.kt
@@ -1,92 +1,45 @@
 package com.tobybook.ch01
 
-import org.springframework.dao.EmptyResultDataAccessException
-import java.sql.Connection
-import java.sql.PreparedStatement
-import java.sql.ResultSet
-import java.sql.SQLException
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.queryForObject
 import javax.sql.DataSource
 
 class UserDAO(var dataSource: DataSource) {
-    var jdbcContext = JdbcContext(dataSource)
 
-    fun add(user: User) {
-        jdbcContext.workWithStatementStrategy(
-            object : StatementStrategy {
-                override fun makePreparedStatement(c: Connection): PreparedStatement {
-                    val ps =
-                        c.prepareStatement("insert into users(id, name, password) values (?, ?, ?)")
+    var jdbcTemplate: JdbcTemplate = JdbcTemplate(dataSource)
 
-                    ps.setString(1, user.id)
-                    ps.setString(2, user.name)
-                    ps.setString(3, user.password)
-
-                    return ps
-                }
-            }
+    fun add(user: User) =
+        jdbcTemplate.update(
+            "insert into users(id, name, password) values(?, ? , ?)",
+            user.id,
+            user.name,
+            user.password
         )
-    }
 
-    fun get(id: String): User {
-        var c: Connection? = null
-        var ps: PreparedStatement? = null
-        var rs: ResultSet? = null
+    fun get(id: String): User = jdbcTemplate.queryForObject<User>(
+            "select * from users where id = ?",
+            arrayOf<Any>(id)
+        ) { rs, rowNum ->
+            User(
+                rs.getString("id"),
+                rs.getString("name"),
+                rs.getString("password")
+            )
+        }!!
 
-        try {
-            c = dataSource.connection
-            ps = c.prepareStatement("select * from users where id = ?")
+    fun deleteAll() =
+        jdbcTemplate.update { con -> con.prepareStatement("delete from users") }
 
-            ps.setString(1, id)
+    fun getCount(): Int =
+        jdbcTemplate.queryForObject("select count(*) from users")
 
-            rs = ps.executeQuery()
-
-            var user: User? = null
-
-            if (rs.next()) {
-                user = User(
-                    rs.getString("id"),
-                    rs.getString("name"),
-                    rs.getString("password")
-                )
-            }
-
-            ps.close()
-            rs.close()
-            c.close()
-
-            return user ?: throw EmptyResultDataAccessException(1)
-        } catch (e: SQLException) {
-            throw e
-        } finally {
-            if (rs != null) { try { rs.close() } catch (e: SQLException) { } }
-            if (ps != null) { try { ps.close() } catch (e: SQLException) { } }
-            if (c != null) { try { c.close() } catch (e: SQLException) { } }
+    fun getAll(): List<User> = jdbcTemplate.query(
+            "select * from users order by id desc",
+        ) { rs, rowNum ->
+            User(
+                rs.getString("id"),
+                rs.getString("name"),
+                rs.getString("password")
+            )
         }
-    }
-
-    fun deleteAll() {
-        jdbcContext.executeSql("delete from users")
-    }
-
-    fun getCount(): Int {
-        var c:Connection? = null
-        var ps: PreparedStatement? = null
-        var rs: ResultSet? = null
-
-        try {
-            c = dataSource.connection
-            ps = c.prepareStatement("select count(*) from users")
-
-            rs = ps.executeQuery()
-            rs.next()
-
-            return rs.getInt(1)
-        } catch(e: SQLException) {
-            throw e
-        } finally {
-            if (rs != null) { try { rs.close() } catch (e: SQLException) { } }
-            if (ps != null) { try { ps.close() } catch (e: SQLException) { } }
-            if (c != null) { try { c.close() } catch (e: SQLException) { } }
-        }
-    }
 }
--- src/test/kotlin/com/tobybook/ch01/UserDAOTest.kt
+++ src/test/kotlin/com/tobybook/ch01/UserDAOTest.kt
@@ -46,4 +46,41 @@ internal class UserDAOTest {
         assertThat(user2.name).isEqualTo(user.name)
         assertThat(user2.password).isEqualTo(user.password)
     }
+
+    @Test
+    fun getAll() {
+        dao.deleteAll()
+
+        val users0 = dao.getAll()
+
+        assertThat(users0.size).isEqualTo(0)
+
+        val user1 = User("1", "훈", "1234")
+        val user2 = User("2", "쮀", "1234")
+        val user3 = User("3", "얀", "1234")
+
+        dao.add(user1)
+        val users1: List<User> = dao.getAll()
+        assertThat(users1.size).isEqualTo(1)
+        checkSameUser(user1, users1[0])
+
+        dao.add(user2)
+        val users2: List<User> = dao.getAll()
+        assertThat(users2.size).isEqualTo(2)
+        checkSameUser(user1, users2[1])
+        checkSameUser(user2, users2[0])
+
+        dao.add(user3)
+        val users3: List<User> = dao.getAll()
+        assertThat(users3.size).isEqualTo(3)
+        checkSameUser(user1, users3[2])
+        checkSameUser(user2, users3[1])
+        checkSameUser(user3, users3[0])
+    }
+
+    private fun checkSameUser(user1: User, user2: User) {
+        assertThat(user1.id).isEqualTo(user2.id)
+        assertThat(user1.name).isEqualTo(user2.name)
+        assertThat(user1.password).isEqualTo(user2.password)
+    }
 }
