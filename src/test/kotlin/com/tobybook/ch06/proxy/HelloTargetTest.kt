6d8e5819af084ba316ffcb12e177df1a276803f7
hoon <moradulee@gmail.com>
2021-12-30 01:05:01 +0900

    ch06: 포인트컷, 어드바이스 학습 테스트

--- src/test/kotlin/com/tobybook/ch06/proxy/HelloTargetTest.kt
+++ src/test/kotlin/com/tobybook/ch06/proxy/HelloTargetTest.kt
@@ -1,10 +1,54 @@
 package com.tobybook.ch06.proxy
 
+import org.aopalliance.intercept.MethodInterceptor
+import org.aopalliance.intercept.MethodInvocation
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.aop.framework.ProxyFactoryBean
+import org.springframework.aop.support.DefaultPointcutAdvisor
+import org.springframework.aop.support.NameMatchMethodPointcut
 import java.lang.reflect.Proxy
 
 internal class HelloTargetTest {
+
+    @Test
+    fun pointcutAdvisor() {
+        val proxyFactoryBean = ProxyFactoryBean()
+        proxyFactoryBean.setTarget(HelloTarget())
+
+        val pointcut = NameMatchMethodPointcut()
+        pointcut.setMappedName("sayH*") //이름 비교조건. sayH로 시작하는 모든 메소드
+
+        //포인트컷과 어드바이스를 Advisor로 묶어서 한 번에 추가
+        proxyFactoryBean.addAdvisor(DefaultPointcutAdvisor(pointcut, UppercaseAdvice()))
+
+        val proxiedHello = proxyFactoryBean.`object` as Hello
+
+        assertThat(proxiedHello.sayHello("borin")).isEqualTo("HELLO BORIN")
+        assertThat(proxiedHello.sayHi("borin")).isEqualTo("HI BORIN")
+        assertThat(proxiedHello.sayThankYou("Borin")).isEqualTo("Thank You Borin")
+    }
+
+    @Test
+    fun proxyFactoryBean() {
+        val proxyFactoryBean = ProxyFactoryBean()
+        proxyFactoryBean.setTarget(HelloTarget())
+        proxyFactoryBean.addAdvice(UppercaseAdvice())
+
+        val proxiedHello = proxyFactoryBean.`object` as Hello
+
+        assertThat(proxiedHello.sayHello("Hoon")).isEqualTo("HELLO HOON")
+        assertThat(proxiedHello.sayHi("hoon")).isEqualTo("HI HOON")
+        assertThat(proxiedHello.sayThankYou("hooN")).isEqualTo("THANK YOU HOON")
+    }
+
+    inner class UppercaseAdvice: MethodInterceptor {
+        override fun invoke(invocation: MethodInvocation): Any? {
+            val ret: String = invocation.proceed() as String
+            return ret.uppercase()
+        }
+    }
+
     @Test
     fun simpleProxy() {
         val hello = HelloTarget()
